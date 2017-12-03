7ac14d6485f9d80f87c5349687ffe6bd59b10a6a
joasiacode <consilis@wp.pl>
2017-12-03 21:54:07 +0100

    change type of methods, split the files

--- game.rb
+++ game.rb
@@ -1,91 +1,129 @@
 class Game
 
-board={A1: " ",B1: " ",C1: " ",A2: " ",B2: " ",C2: " ",A3: " ",B3: " ",C3: " "}
+MAGIC_SQUARE=({A1: 8,B1: 1,C1: 6,A2: 3,B2: 5,C2: 7,A3: 4,B3: 9,C3: 2})
 
-def self.print_board(board)
+def initialize(board, points_x, points_o)
+  @board = board
+  @points_x = points_x
+  @points_o = points_o
+end
+
+def print_board
   puts " | A | B | C |"
   puts " -------------"
-  puts "1| #{board[:A1]} | #{board[:B1]} | #{board[:C1]} |"
+  puts "1| #{@board[:A1]} | #{@board[:B1]} | #{@board[:C1]} |"
   puts " -------------"
-  puts "2| #{board[:A2]} | #{board[:B2]} | #{board[:C2]} |"
+  puts "2| #{@board[:A2]} | #{@board[:B2]} | #{@board[:C2]} |"
   puts " -------------"
-  puts "3| #{board[:A3]} | #{board[:B3]} | #{board[:C3]} |"
+  puts "3| #{@board[:A3]} | #{@board[:B3]} | #{@board[:C3]} |"
   puts " -------------"
 end
 
-def self.consistent_rows?(board)
-  row1 = board[:A1] == board[:B1] && board[:B1] == board[:C1] && board[:A1] != " "
-  row2 = board[:A2] == board[:B2] && board[:B2] == board[:C2] && board[:A2] != " "
-  row3 = board[:A3] == board[:B3] && board[:B3] == board[:C3] && board[:A3] != " "
-  row1||row2||row3
+def x_points_rows?
+  row1 = @points_x[:A1] + @points_x[:B1] + @points_x[:C1]
+  row2 = @points_x[:A2] + @points_x[:B2] + @points_x[:C2]
+  row3 = @points_x[:A3] + @points_x[:B3] + @points_x[:C3]
+  row1==15||row2==15||row3==15
 end
 
-def self.consistent_columns?(board)
-  column1 = board[:A1]==board[:A2]&&board[:A2]==board[:A3] && board[:A1] != " "
-  column2 = board[:B1]==board[:B2]&&board[:B2]==board[:B3] && board[:B1] != " "
-  column3 = board[:C1]==board[:C2]&&board[:C2]==board[:C3] && board[:C1] != " "
-  column1||column2||column3
+def x_points_columns?
+  column1 = @points_x[:A1] + @points_x[:A2] + @points_x[:A3]
+  column2 = @points_x[:B1] + @points_x[:B2] + @points_x[:B3]
+  column3 = @points_x[:C1] + @points_x[:C2] + @points_x[:C3]
+  column1==15||column2==15||column3==15
 end
 
-def self.consistent_diagonals?(board)
-  diagonal1 = board[:A1]==board[:B2]&&board[:B2]==board[:C3] && board[:A1] != " "
-  diagonal2 = board[:C1]==board[:B2]&&board[:B2]==board[:A3] && board[:C1] != " "
-  diagonal1||diagonal2
+def x_points_diagonals?
+  diagonal1 = @points_x[:A1] + @points_x[:B2] + @points_x[:C3]
+  diagonal2 = @points_x[:C1] + @points_x[:B2] + @points_x[:A3]
+  diagonal1==15||diagonal2==15
 end
 
-def self.victory?(board)
-  consistent_rows?(board)||consistent_columns?(board)||consistent_diagonals?(board)
+def x_victory?
+  x_points_rows?||x_points_columns?||x_points_diagonals?
 end
 
-def self.move_x(board)
-  input=gets.chomp.upcase
-  if board[input.to_sym]==" "
-    board[input.to_sym]="X"
-    print_board(board)
+def o_points_rows?
+  row1 = @points_o[:A1] + @points_o[:B1] + @points_o[:C1]
+  row2 = @points_o[:A2] + @points_o[:B2] + @points_o[:C2]
+  row3 = @points_o[:A3] + @points_o[:B3] + @points_o[:C3]
+  row1==15||row2==15||row3==15
+end
+
+def o_points_columns?
+  column1 = @points_o[:A1] + @points_o[:A2] + @points_o[:A3]
+  column2 = @points_o[:B1] + @points_o[:B2] + @points_o[:B3]
+  column3 = @points_x[:C1] + @points_x[:C2] + @points_x[:C3]
+  column1==15||column2==15||column3==15
+end
+
+def o_points_diagonals?
+  diagonal1 = @points_o[:A1] + @points_o[:B2] + @points_o[:C3]
+  diagonal2 = @points_o[:C1] + @points_o[:B2] + @points_o[:A3]
+  diagonal1==15||diagonal2==15
+end
+
+def o_victory?
+  o_points_rows?||o_points_columns?||o_points_diagonals?
+end
+
+def victory?
+  x_victory?||o_victory?
+end
+
+def move_x
+  input = gets.chomp.upcase!
+  if @board[input.to_sym]==" "
+    @board[input.to_sym]="X"
+    @points_x[input.to_sym]=Game::MAGIC_SQUARE[input.to_sym]
+    print_board
   else
     puts "Wrong field, give another"
-    move_x(board)
+    move_x
   end
 end
 
-def self.move_o(board)
-  input=gets.chomp.upcase
-  if board[input.to_sym]==" "
-    board[input.to_sym]="O"
-    print_board(board)
+def move_o
+  input = gets.chomp.upcase!
+  if @board[input.to_sym]==" "
+    @board[input.to_sym]="O"
+    @points_o[input.to_sym]=Game::MAGIC_SQUARE[input.to_sym]
+    print_board
   else
     puts "Wrong field, give another"
-    move_o(board)
+    move_o
   end
 end
 
-def self.play(board)
-  print_board(board)
+def play
+  print_board
 
   i=1
   j=1
 
+
   5.times do
-    unless victory?(board)
+    unless victory?
       puts "X-Move #{i}"
       i=i+1
-      move_x(board)
-      unless victory?(board)||j==5
+      move_x
+      unless victory?||j==5
         puts "O-Move #{j}"
         j=j+1
-        move_o (board)
+        move_o
       end
     end
   end
 
-  if victory?(board)
-    puts "VICTORY"
+  if x_victory?
+    puts "VICTORY!!! THE WINNER IS... X-PLAYER"
+  elsif o_victory?
+    puts "VICTORY!!! THE WINNER IS... O-PLAYER"
   else
     puts "GAME OVER"
   end
 
 end
 
-play(board)
 
 end
