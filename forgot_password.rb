7b9345ec0ca0550152b583c19f7c4c8407e3effc
joasiacode <consilis@wp.pl>
2018-01-14 23:22:22 +0100

    fixes by code review

--- forgot_password.rb
+++ forgot_password.rb
@@ -1,15 +1,16 @@
 require 'digest'
 
 def store_new_password(filename, string)
+  File.open(filename, "w")
   File.open(filename, "a+") do |file|
-    file.truncate(0)
     file.puts(string)
   end
 end
 
+
 def hashed_password(user_password)
-  salt = "e04430f2a1d2cc02ea82"
-  timestamp = "1507282890"
+  salt = (('0'..'9').to_a+(';'..'z').to_a).shuffle.first(20).join
+  timestamp = Time.now.to_i.to_s
   hashed_password = Digest::SHA2.new(512).hexdigest(salt+"#"+user_password.to_s+"#"+timestamp)
   salt+":"+hashed_password+":"+timestamp
 end
@@ -23,8 +24,8 @@ if input == input.strip
     input2 = gets.chomp
 
     if input == input2
-      string =  hashed_password(input2.to_s)
-      store_new_password("password.txt",string)
+      string =  hashed_password(input2)
+      store_new_password("password2.txt",string)
     else
       puts "Hasła się nie zgadzają"
     end
--- logged_with_sinatra/app.rb
+++ logged_with_sinatra/app.rb
@@ -1,3 +1,66 @@
+
+require 'sinatra'
+require 'bundler'
+require 'digest'
+
+
+Bundler.require(:default)
+
+require 'sinatra/reloader'
+
+enable :sessions
+
+FILE = "password.txt"
+SALT = File.read(FILE).split(":").first
+TIMESTAMP = File.read(FILE).split(":").last
+PASSWORD = read_password
+
+
+def hashed_password(user_password)
+  hashed_password = Digest::SHA2.new(512).hexdigest(SALT+"#"+user_password.to_s+"#"+TIMESTAMP)
+  salt+":"+hashed_password+":"+timestamp
+end
+
+def read_password
+  file = FILE
+  return [] unless File.exist?(file)
+  File.read(file)
+end
+
+get "/" do
+
+  if session[:logged] = false
+    @user_password = params[:user_password]
+    erb :index
+  else
+    redirect  "/logged"
+  end
+
+end
+
+post "/" do
+  @user_password = hashed_password(params[:user_password])
+
+  if @user_password == PASSWORD
+    session[:logged] = true
+    redirect "/logged"
+  else
+    redirect "/"
+  end
+end
+
+get '/logged' do
+  if session[:logged]
+    erb :logged
+  else
+    redirect "/"
+  end
+end
+
+post "/logged" do
+  session[:logged] = false
+  redirect "/"
+end
 # myapp.rb
 require 'sinatra'
 require 'bundler'
